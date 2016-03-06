73265c0e1f1c660620257a16a9a07e90f46c8def
Ian <ianmdawson@gmail.com>
2016-03-05 16:13:54 -0800

    account for using oh-my-zsh

---
+++ oh-my-zsh/install.sh
@@ -0,0 +1,4 @@
+if [[ ! -a ~/.oh-my-zsh ]]
+then
+  sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
---
+++ oh-my-zsh/oh-my-sh.zsh
@@ -0,0 +1,5 @@
+# oh-my-zsh config
+ZSH_THEME="steeef"
+plugins=(git wd)
+
+source ~/.oh-my-zsh/oh-my-zsh.sh
