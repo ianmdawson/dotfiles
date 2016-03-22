cff290e0dc1990a4906fb33d91d87d6f8e464025
Ian Dawson <idawson@shyp.com>
2016-03-21 19:35:30 -0700

    add info on custom oh-my-zsh folders

--- oh-my-zsh/oh-my-sh.zsh
+++ oh-my-zsh/oh-my-sh.zsh
@@ -3,7 +3,9 @@ ZSH_THEME="steeef"
 plugins=(git wd)
 DISABLE_UPDATE_PROMPT="true"
 
-# custom prompt based on steeef
+# custom prompt based on steeef, maybe some day do this:
+# https://github.com/robbyrussell/oh-my-zsh/wiki/Customization#using-another-customization-directory
+
 PROMPT=$'
 %{$purple%}%n${PR_RST} at %{$orange%}%m${PR_RST} in %{$limegreen%}%~${PR_RST} $vcs_info_msg_0_$(virtualenv_info) %{$purple%}%*${PR_RST}
 $ '
