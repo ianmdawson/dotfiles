c45d88f1c9ef27759dc4cb1b016b8fc777eb20e7
Ian Dawson <idawson@shyp.com>
2016-03-21 19:32:51 -0700

    add custom prompt

--- oh-my-zsh/oh-my-sh.zsh
+++ oh-my-zsh/oh-my-sh.zsh
@@ -1,7 +1,11 @@
 # oh-my-zsh config
 ZSH_THEME="steeef"
 plugins=(git wd)
-RPROMPT="%*"
 DISABLE_UPDATE_PROMPT="true"
+
+# custom prompt based on steeef
+PROMPT=$'
+%{$purple%}%n${PR_RST} at %{$orange%}%m${PR_RST} in %{$limegreen%}%~${PR_RST} $vcs_info_msg_0_$(virtualenv_info) %{$purple%}%*${PR_RST}
+$ '
 
 source ~/.oh-my-zsh/oh-my-zsh.sh
