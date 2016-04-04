b93ba858a8655500597a89b41e4abedb9ef95a9b
Ian M Dawson <idawson@shyp.com>
2016-04-04 11:49:53 -0700

    move update-osx outside of install logic

--- script/update-osx.sh
+++ script/update-osx.sh
@@ -1,14 +1,18 @@
+#!/usr/bin/env bash
+
 # The Brewfile handles Homebrew-based app and library installs, but there may
 # still be updates and installables in the Mac App Store. There's a nifty
 # command line interface to it that we can use to just install everything, so
 # yeah, let's do that.
 
 echo "› sudo softwareupdate -i -a"
-while true; do
-    read -p "Do you wish to install this program? " yn
-    case $yn in
-        [Yy]* ) sudo softwareupdate -i -a; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer y or n.";;
-    esac
-done
+
+read -p "Check for OS X updates (y/n)? " answer
+case ${answer:0:1} in
+    y|Y )
+        sudo softwareupdate -i -a
+    ;;
+    * )
+        echo "Moving on"
+    ;;
+esac
