517688d283863b74308d70c27b2a037db1b5ef2e
Ian <ianmdawson@gmail.com>
2016-09-27 19:44:29 -0700

    turn down key repeat for osx sierra

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -21,7 +21,8 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 chflags nohidden ~/Library
 
 # Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 0
+# broken in macOS sierra :(
+# defaults write NSGlobalDomain KeyRepeat -int 0
 
 # Set the Finder prefs for showing a few different volumes on the Desktop.
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
