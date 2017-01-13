990b12637d01e48a55d6ada78c3db2420320435b
Ian M Dawson <idawson@shyp.com>
2017-01-13 10:52:01 -0800

    add fzf to oh-my-zsh

--- oh-my-zsh/oh-my-sh.zsh
+++ oh-my-zsh/oh-my-sh.zsh
@@ -1,6 +1,6 @@
 # oh-my-zsh config
 ZSH_THEME="steeef-mod"
-plugins=(git wd)
+plugins=(git wd fzf-zsh)
 DISABLE_UPDATE_PROMPT="true"
 DISABLE_AUTO_UPDATE="true"
 ZSH_CUSTOM="${DOTFILES}/oh-my-zsh/customization"
