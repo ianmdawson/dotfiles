b117285f16dca757cc3bdcb5c757afecfb9e3864
Ian M Dawson <idawson@shyp.com>
2017-01-13 10:12:34 -0800

    turn off oh-my-zsh autoupdate now that it's in dot

--- oh-my-zsh/oh-my-sh.zsh
+++ oh-my-zsh/oh-my-sh.zsh
@@ -2,6 +2,7 @@
 ZSH_THEME="steeef-mod"
 plugins=(git wd)
 DISABLE_UPDATE_PROMPT="true"
-ZSH_CUSTOM=$DOTFILES/oh-my-zsh/customization
+DISABLE_AUTO_UPDATE="true"
+ZSH_CUSTOM="${DOTFILES}/oh-my-zsh/customization"
 
 source ~/.oh-my-zsh/oh-my-zsh.sh
