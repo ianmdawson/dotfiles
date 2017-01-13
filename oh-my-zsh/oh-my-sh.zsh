506dfe214e98df938d8d892984daefa6eade24be
Ian M Dawson <idawson@shyp.com>
2017-01-13 12:47:55 -0800

    remove fzf from zsh for now

--- oh-my-zsh/oh-my-sh.zsh
+++ oh-my-zsh/oh-my-sh.zsh
@@ -1,6 +1,6 @@
 # oh-my-zsh config
 ZSH_THEME="steeef-mod"
-plugins=(git wd fzf-zsh)
+plugins=(git wd)
 DISABLE_UPDATE_PROMPT="true"
 DISABLE_AUTO_UPDATE="true"
 ZSH_CUSTOM="${DOTFILES}/oh-my-zsh/customization"
