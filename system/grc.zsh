a298798b9eb16345d50e802ae787b9869c60923f
Ian M. Dawson <ianmdawson@gmail.com>
2017-02-12 19:50:50 -0800

    fix grc / colourify

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,30 @@
 # GRC colorizes nifty unix tools all over the place
 if (( $+commands[grc] )) && (( $+commands[brew] ))
 then
-  source `brew --prefix`/etc/grc.bashrc
+  # Instead of souricng this, it is copied from the following file so changes
+  # be made to it:
+  # source `brew --prefix`/etc/grc.bashrc
+  GRC=`which grc`
+  if [ "$TERM" != dumb ] && [ -n "$GRC" ]
+  then
+      alias colourify="$GRC -es --colour=auto"
+      # alias colourifyconfigure='colourify -c `brew --prefix`/etc/configure'
+      alias diff='colourify diff'
+      alias make='colourify make'
+      alias gcc='colourify gcc'
+      alias g++='colourify g++'
+      alias as='colourify as'
+      alias gas='colourify gas'
+      alias ld='colourify ld'
+      alias netstat='colourify netstat'
+      alias ping='colourify ping'
+      alias traceroute='colourify /usr/sbin/traceroute'
+      alias head='colourify head'
+      alias tail='colourify tail'
+      alias dig='colourify dig'
+      alias mount='colourify mount'
+      alias ps='colourify ps'
+      alias mtr='colourify mtr'
+      alias df='colourify df'
+  fi
 fi
