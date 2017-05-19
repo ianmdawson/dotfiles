936b32cd52edbba4d55352c28cc2c75f23b43f44
Ian M. Dawson <idawson@shyp.com>
2017-05-19 11:10:47 -0700

    load zprof, ignore duplicate history entries

--- oh-my-zsh/oh-my-sh.zsh
+++ oh-my-zsh/oh-my-sh.zsh
@@ -4,5 +4,9 @@ plugins=(git wd)
 DISABLE_UPDATE_PROMPT="true"
 DISABLE_AUTO_UPDATE="true"
 ZSH_CUSTOM="${DOTFILES}/oh-my-zsh/customization"
+HIST_IGNORE_DUPS="true" # don't enter duplicate entries into history
+
+# load profiling
+zmodload zsh/zprof
 
 source ~/.oh-my-zsh/oh-my-zsh.sh
