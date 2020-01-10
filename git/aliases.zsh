fd1c3ce675fd9bbd9893fca5a06bf289ffcc9dc0
Ian M. Dawson <ianmdawson@gmail.com>
2020-01-10 12:49:51 -0800

    add a master...develop comparison alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,6 +4,7 @@ hub_path=$(which hub)
 if (( $+commands[hub] ))
 then
   alias git=$hub_path
+  alias check='hub compare master...develop'
 fi
 
 alias gloga="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
