bb63d85c5f3b902ee3d5d639565e7776a4645df0
Ian M. Dawson <ianmdawson@gmail.com>
2020-05-20 18:59:44 -0700

    zprof off for now

--- oh-my-zsh/oh-my-sh.zsh
+++ oh-my-zsh/oh-my-sh.zsh
@@ -4,9 +4,10 @@ plugins=(git wd)
 DISABLE_UPDATE_PROMPT="true"
 DISABLE_AUTO_UPDATE="true"
 ZSH_CUSTOM="${DOTFILES}/oh-my-zsh/customization"
-HIST_IGNORE_DUPS="true" # don't enter duplicate entries into history
+# HIST_IGNORE_DUPS=true # don't enter duplicate entries into history
+# HIST_IGNORE_ALL_DUPS=false      # Delete old recorded entry if new entry is a duplicate.
 
 # load profiling
-zmodload zsh/zprof
+# zmodload zsh/zprof
 
 source ~/.oh-my-zsh/oh-my-zsh.sh
